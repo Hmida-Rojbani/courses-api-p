6723554160f171008c5e88d9f0adc43410448bf0
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-29 18:34:09 +0100

    add express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 require('./db_con')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, () => console.log(`Server on ${port}`));
