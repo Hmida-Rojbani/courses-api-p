25808f47a87784de7aecc1af31e319b32b49eab3
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-29 19:25:04 +0100

    add get and post

---
+++ routers/courses.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Course = require('../models/course');
+const _ =require('lodash')
+
+
+router.get('',async (req,res)=>{
+    let courses = await Course.find();
+    res.send(courses)
+})
+
+router.post('',async (req,res)=>{
+    let course = new Course(_.pick(req.body,['title','author','tags','price','isPublished']));
+    course = await course.save();
+    res.status(201).send(course)
+})
+
+module.exports=router
